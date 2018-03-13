882909ab957f42b077cc64a51255b487b57e48b1
Adam <mail@adamkhafagy.com>
2018-03-13 15:27:06 -0400

    Added Oauth

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
+var stripe = require('stripe')('sk_test_r5djZLubk2F9JBONkPSP6fMc');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Project Lyra' });
+});
+
+router.get('/token', function(req,res, next){
+  res.render('success', { title: 'Lyra' });
 });
 
 module.exports = router;
